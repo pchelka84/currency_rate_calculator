0315eb0de2790f2b4887c9fd022f7dbb45860f44
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-17 20:33:38 -0400

    Handle calculate rate functionality

--- script.js
+++ script.js
@@ -7,7 +7,20 @@ const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
 // Fetch exchange rates and update the DOM
-function calculate() {}
+function calculate() {
+  const currencyOne = currencyEl_one.value;
+  const currencyTwo = currencyEl_two.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne}`)
+    .then(res => res.json())
+    .then(data => {
+    // console.log(data);
+    const rate = data.rates[currencyTwo];
+    
+    rateEl.innerText = `1 ${currencyOne} = ${rate} ${currencyTwo}`;
+    amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+  })
+}
 
 // Event listeners
 currencyEl_one.addEventListener('change', calculate);
